c6f66554291fa3475c0bd06b7c29afbed2d99e41
psharma501 <130492685+psharma501@users.noreply.github.com>
2023-04-30 10:16:33 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,13 @@
+var items = document.getElementsByClassName('list-group-item');
+items[1].textContent = 'Hello';
+console.log(items[2]);
+
+items[2].textContent = 'Hello';
+items[4].textContent = 'HELLO';
+
+var items = document.getElementsByTagName('li');
+
+console.log(items[4]);
+
+items[4].textContent = 'HELLO';
+
